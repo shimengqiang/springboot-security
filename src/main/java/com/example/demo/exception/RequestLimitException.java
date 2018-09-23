a5f243eab7e305e1e4473e6fc72f6a9f0bed4bc5
shimengqiang <294873269@qq.com>
2018-09-23 10:45:58 +0800

    exception

---
+++ src/main/java/com/example/demo/exception/RequestLimitException.java
@@ -0,0 +1,18 @@
+package com.example.demo.exception;
+
+/**
+ * <p>创建时间: 2018/9/23 9:32 </p>
+ *
+ * @author shimengqiang
+ * @version v1.0
+ */
+public class RequestLimitException extends Exception{
+
+        public RequestLimitException() {
+            super("HTTP请求超出设定的限制");
+        }
+
+        public RequestLimitException(String message) {
+            super(message);
+        }
+}
