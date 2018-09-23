7ca8a88d6dab599a6f810d9181d6cf7cb4d45be4
shimengqiang <294873269@qq.com>
2018-09-23 10:44:28 +0800

    get real ip

---
+++ src/main/java/com/example/demo/utils/HttpRequestUtil.java
@@ -0,0 +1,34 @@
+package com.example.demo.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>创建时间: 2018/9/23 9:44 </p>
+ *
+ * @author shimengqiang
+ * @version v1.0
+ */
+public class HttpRequestUtil {
+
+
+    public static String getIpAddress(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_CLIENT_IP");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
+}
